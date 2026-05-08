97992f926786d29b27162754498b79a6dcbfaa17
Dovhal Illia <dovhal.illia21@gmail.com>
2026-05-08 17:04:11 +0200

    feat(CompilerError): add new ErrorStage types

--- include/common/CompilerError.h
+++ include/common/CompilerError.h
@@ -6,14 +6,17 @@
 
 /// @brief Compilation stage when an error occured
 enum class ErrorStage {
-  LEXER,  ///< Error occurred during lexical analysis
-  PARSER, ///< Error occurred during parsing
-  CODEGEN ///<  Error occurred during code generation
+  LEXER,          ///< Error occurred during lexical analysis
+  PARSER,         ///< Error occurred during parsing
+  GROUPER,        ///< Error occured during grouper
+  ENCODER,        ///< Error occured during encoder
+  SIZE_RESOLVER,  ///< Error occured during size solver
+  ELF_WRITER      ///<  Error occurred during elf writer
 };
 
 /// @brief Represents a compiler error with location and message
 struct CompilerError {
-  ErrorStage stage;    ///< Compilation stage where the error happened
-  SourceSpan span;     ///< Source location of the error
-  std::string message; ///< Human-readable error description
+  ErrorStage stage;     ///< Compilation stage where the error happened
+  SourceSpan span;      ///< Source location of the error
+  std::string message;  ///< Human-readable error description
 };
